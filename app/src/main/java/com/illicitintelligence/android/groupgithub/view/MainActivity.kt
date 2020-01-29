c12f5e1608010851bb7b7dee796776e4f0c8bf5a
Chris FitzGerald <58571528+chrisfitz4@users.noreply.github.com>
2020-01-29 08:08:42 -0500

    test

--- app/src/main/java/com/illicitintelligence/android/groupgithub/view/MainActivity.kt
+++ app/src/main/java/com/illicitintelligence/android/groupgithub/view/MainActivity.kt
@@ -3,13 +3,19 @@ package com.illicitintelligence.android.groupgithub.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.illicitintelligence.android.groupgithub.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
-        //here is my comment to commit
+
+        val splashFragment = SplashFragment()
+        supportFragmentManager.beginTransaction()
+            .setCustomAnimations(R.anim.expand_center,R.anim.collapse_center,R.anim.expand_center,R.anim.collapse_center)
+            .addToBackStack(splashFragment.tag)
+            .add(R.id.frame_layout,splashFragment)
+            .commit()
     }
 }
