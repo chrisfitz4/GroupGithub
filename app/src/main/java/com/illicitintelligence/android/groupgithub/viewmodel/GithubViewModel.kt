baaca951d99f648f4d3fc2ed1ed93b34cf219888
trung-luu-enhance <trung.luu@enhanceit.us>
2020-01-29 15:43:42 -0500

    Fixed GitViewModel based on Chris's suggestion

--- app/src/main/java/com/illicitintelligence/android/groupgithub/viewmodel/GithubViewModel.kt
+++ app/src/main/java/com/illicitintelligence/android/groupgithub/viewmodel/GithubViewModel.kt
@@ -2,8 +2,12 @@ package com.illicitintelligence.android.groupgithub.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.illicitintelligence.android.groupgithub.model.GithubRepos
+import com.illicitintelligence.android.groupgithub.model.GithubUser
 import com.illicitintelligence.android.groupgithub.network.GithubRetrofit
 import com.illicitintelligence.android.groupgithub.network.UserAccessToken
+import io.reactivex.Observable
+import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
@@ -13,34 +17,22 @@ class GithubViewModel (application: Application) : AndroidViewModel(application)
     private val githubRetrofit = GithubRetrofit()
     private val compositeDisposable = CompositeDisposable()
 
-    fun getUser(username: String) {
-        compositeDisposable.add(
-            githubRetrofit.getUser(username)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe { gitUSer ->
-
-                })
+    fun getUser(username: String): Observable<GithubUser>? {
+        return githubRetrofit.getUser(username)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
     }
 
-    fun getRepos(username: String) {
-        compositeDisposable.add(
-            githubRetrofit.getRepos(username)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe { list ->
-
-                })
+    fun getRepos(username: String): Observable<List<GithubRepos>>? {
+        return githubRetrofit.getRepos(username)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
     }
 
-    fun getRepos(userAccessToken: UserAccessToken) {
-        compositeDisposable.add(
-            githubRetrofit.getRepos(userAccessToken)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe { list ->
-
-                })
+    fun getRepos(userAccessToken: UserAccessToken): Observable<List<GithubRepos>>? {
+        return githubRetrofit.getRepos(userAccessToken)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
     }
 
 }
